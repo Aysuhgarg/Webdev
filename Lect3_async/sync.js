346fccc850fa8d45a535ef11596d6b3c6b162063
Aysuhgarg <Coolayushgarg222@gmail.com>
2021-06-10 22:49:28 +0530

    sync

--- Lect3_async/sync.js
+++ Lect3_async/sync.js
@@ -8,4 +8,7 @@ console.log("start");
 let f1KaData = fs.readFileSync("./f1.txt" , "utf8"); // 100gb
 console.log(f1KaData);
 
+var f1ka=fs.readFileSync("./f2.txt");
+console.log(f1ka+" ");
+
 console.log("end");
