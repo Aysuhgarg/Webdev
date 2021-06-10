e64b370c6d397a62d8bd27033eccb5e3c88db3d9
Aysuhgarg <Coolayushgarg222@gmail.com>
2021-06-10 23:46:55 +0530

    nfiles

--- Lect3_async/nFiles/asyncParallel.js
+++ Lect3_async/nFiles/asyncParallel.js
@@ -1,2 +1,12 @@
 let fs = require("fs");
 let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
+
+for(let i=0;i<files.length;i++)
+{
+    fs.readFile(files[i],fun);
+}
+
+function fun(error,data)
+{
+    console.log(data+" ");
+}
--- Lect3_async/nFiles/asyncSerial.js
+++ Lect3_async/nFiles/asyncSerial.js
@@ -1,16 +1,3 @@
-// let fs = require("fs");
-// let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
-
-// for(let i=0;i<files.length;i++)
-// {
-//     fs.readFile(files[i],fun);
-// }
-
-// function fun(error,data)
-// {
-//     console.log(data+" ");
-// }
-
 
 let fs = require("fs");
 let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
