6400e6f972088c832b147347e9544252dfc4610a
Aysuhgarg <Coolayushgarg222@gmail.com>
2021-06-10 22:29:28 +0530

    lecture 2nd

---
+++ Lecture2/memory.js
@@ -0,0 +1,30 @@
+console.log(multiply);
+
+function multiply(a,b){
+    return a*b;
+}
+
+console.log(add);
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(x);
+
+var x = multiply(10,10);
+
+console.log(y);
+
+var y = add(x , 20);
+
+console.log(x);
+console.log(y);
+
+// Hoisting =>
+
+console.log(a);
+var a = 20;
+
+// console.log(b);
+// let b = 20;
