968cf87e1cb0cbcb8846bc03c71ea5499eb69e52
Aysuhgarg <Coolayushgarg222@gmail.com>
2021-06-11 21:56:31 +0530

    install npm

---
+++ Lec4/facts/fs.js
@@ -0,0 +1,35 @@
+// go to niet folder location in terminal
+// npm init -y
+// npm i cheerio
+// add node_modules in .gitignore
+
+
+const fs = require("fs");
+const cheerio = require("cheerio");
+
+
+let htmlKaData = fs.readFileSync("./index.html" , "utf8");
+
+//console.log(htmlKaData);
+// jquery
+let ch = cheerio.load(htmlKaData);
+// let pTagKaData = ch("p").text();
+// <p>Hey this is a p tag !!</p>
+ //console.log(pTagKaData);
+
+// get all the matching p tags inside a tag
+let pTagInsideUl = ch("ul p").text();
+// console.log(pTagInsideUl)
+
+
+// direct children
+let directPTag = ch("ul>p").text();
+// console.log(directPTag);
+
+// classes and ids
+
+// i want main p tags
+let mainPTags = ch(".mainp").text();
+console.log(mainPTags);
+
+//  ids => #main
