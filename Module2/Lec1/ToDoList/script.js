48890cd79667bb3e3ac79583e58b41542f35f311
Aysuhgarg <Coolayushgarg222@gmail.com>
2021-06-21 20:47:41 +0530

    deletion on double click

--- Module2/Lec1/ToDoList/script.js
+++ Module2/Lec1/ToDoList/script.js
@@ -4,13 +4,17 @@ let ul=document.querySelector('#ul');
 btn.addEventListener("click",function(){
      //input box se value nikal rhe h
     let data=int.value;
-
+    if(data=="") return ;
     //ek new li element create kr rhe hai
     let li=document.createElement("li");
 
     //input box se jo value nikali thi wo is li element me dal rhe hai
     li.innerText=data;
 
+    //listener on each li object
+    li.addEventListener("dblclick",function(){
+    li.remove();
+    });
     //   us li element ko jo abhi create kra just ul ke ander dal rhe hai
     ul.append(li);
     // wapas se input box ko blank kr rhe hai
